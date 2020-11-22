3b6eb16ca829d29468fd4e92e2bea3297fe103c5
Oleg <kolyushko.oleg@gmail.com>
2020-11-22 21:18:54 +0200

    Updates

---
+++ precache-manifest.f72445f2410e047ecb5f2203704784b1.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8edb7c10ac5abfad381176cb2a50bf11",
+    "url": "/goit-react-hw-08-phonebook/index.html"
+  },
+  {
+    "revision": "8e7a4999fc086f95bf66",
+    "url": "/goit-react-hw-08-phonebook/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "29f3c673e7777a8b68c7",
+    "url": "/goit-react-hw-08-phonebook/static/css/3.9ac4c5b9.chunk.css"
+  },
+  {
+    "revision": "56869b4ec1a37b2d782a",
+    "url": "/goit-react-hw-08-phonebook/static/css/4.369cc355.chunk.css"
+  },
+  {
+    "revision": "f03732cb55af4f4a3174",
+    "url": "/goit-react-hw-08-phonebook/static/css/5.369cc355.chunk.css"
+  },
+  {
+    "revision": "b194f4a7b4027ec6e832",
+    "url": "/goit-react-hw-08-phonebook/static/css/main.2ee6f4d1.chunk.css"
+  },
+  {
+    "revision": "8e7a4999fc086f95bf66",
+    "url": "/goit-react-hw-08-phonebook/static/js/2.b52366b1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-08-phonebook/static/js/2.b52366b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29f3c673e7777a8b68c7",
+    "url": "/goit-react-hw-08-phonebook/static/js/3.6056f6e0.chunk.js"
+  },
+  {
+    "revision": "56869b4ec1a37b2d782a",
+    "url": "/goit-react-hw-08-phonebook/static/js/4.b79f4814.chunk.js"
+  },
+  {
+    "revision": "f03732cb55af4f4a3174",
+    "url": "/goit-react-hw-08-phonebook/static/js/5.e1ddaebf.chunk.js"
+  },
+  {
+    "revision": "da55f5495cbf510cb35b",
+    "url": "/goit-react-hw-08-phonebook/static/js/6.e202019f.chunk.js"
+  },
+  {
+    "revision": "b194f4a7b4027ec6e832",
+    "url": "/goit-react-hw-08-phonebook/static/js/main.ed32af06.chunk.js"
+  },
+  {
+    "revision": "006b071de931db4f17f2",
+    "url": "/goit-react-hw-08-phonebook/static/js/runtime-main.554f198e.js"
+  },
+  {
+    "revision": "ff3d7423dfa07a77257930c641c740d1",
+    "url": "/goit-react-hw-08-phonebook/static/media/user.ff3d7423.png"
+  }
+]);
